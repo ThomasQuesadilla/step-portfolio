ef8c4a8edac0f6483f95db20c4447b61697b7e5e
Thomas Quintanilla <quintanillathomas00@gmail.com>
2020-06-08 18:09:53 +0000

    remove repetitive function in script.js, fix coordinates to map

--- portfolio/src/main/webapp/mapScript.js
+++ portfolio/src/main/webapp/mapScript.js
@@ -15,6 +15,6 @@
 
 function createMap() {
   const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+    document.getElementById('map'),
+    { center: { lat: 26.0321, lng: -80.2627 }, zoom: 16 });
 }
